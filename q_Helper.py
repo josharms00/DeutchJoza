357973b05ac6b44f483273e8f20458307470fe66
josharms00 <josharms00@gmail.com>
2020-03-20 17:12:17 -0400

    Added function to return the backend with the least amount of queued jobs.

--- q_Helper.py
+++ q_Helper.py
@@ -5,9 +5,11 @@ from qiskit.tools.monitor import job_monitor
 from qiskit.tools.visualization import plot_bloch_multivector 
 from qiskit.visualization import plot_histogram
 
+# class to set up and run quantum circuits
 class q_Helper(object):
 
-	def __init__(self, first=false):
+	# initialize the provider and load account
+	def __init__(self, first=False):
 		if first:
 			IBMQ.save_account(open("token.txt", "r").read())
 		else:
@@ -15,14 +17,35 @@ class q_Helper(object):
 
 		self.provider = IBMQ.get_provider("ibm-q")
 
-	def do_job(self, circuit, shots, backend="ibmq_qasm_simulator", local_sim=false):
+	# does all the work for executing a job for a circuit
+	# returns the results along with the counts
+	def do_job(self, circuit, shots, backend="ibmq_qasm_simulator", local_sim=False):
 		if local_sim:
 			self.provider = Aer.get_backend("qasm_simulator")
 		else:
 			self.backend = self.provider.get_backend(backend)
 
-		self.job = execute(circuit, backend=slef.backend, shots=shots)
+		self.job = execute(circuit, backend=self.backend, shots=shots)
 
 		self.result = self.job.result()
 
 		return self.result, self.result.get_counts()
+
+	# returns the backend with the least amount of queued jobs
+	# can also filter the ones that have less than a specific 
+	# amount of qubits
+	def least_queued(self, qubits=0):
+
+		least_back = self.provider.get_backend("ibmq_16_melbourne")
+		prev_back = least_back
+
+		for backend in self.provider.backends():
+			try:
+				if prev_back.status().pending_jobs > backend.status().pending_jobs and len(backend.properties().qubits) > qubits:
+					least_back = backend
+
+				prev_back = backend
+			except:
+				continue
+				
+		return least_back
